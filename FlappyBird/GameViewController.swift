6a432adbcaf83e73814dd321036b3b6a9c027f4a
Masaru Gushiken <gushiken@zigexn.co.jp>
2014-10-28 12:19:54 +0900

    twitter部分削除

--- FlappyBird/GameViewController.swift
+++ FlappyBird/GameViewController.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import SpriteKit
-import Social
 
 extension SKNode {
     class func unarchiveFromFile(file : NSString) -> SKNode? {
@@ -26,9 +25,6 @@ extension SKNode {
 }
 
 class GameViewController: UIViewController {
-  var myComposeView : SLComposeViewController!
-  var myTwitterButton: UIButton!
-  
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -46,28 +42,6 @@ class GameViewController: UIViewController {
             
             skView.presentScene(scene)
           
-            // twitterに似た色合いをコードで作る
-            let hex: Int = 0x55ACEE
-            let red = Double((hex & 0xFF0000) >> 16) / 255.0
-            let green = Double((hex & 0xFF00) >> 8) / 255.0
-            let blue = Double((hex & 0xFF)) / 255.0
-            var myColor: UIColor = UIColor( red: CGFloat(red), green: CGFloat(green), blue: CGFloat(blue), alpha: CGFloat(1.0))
-            
-            // ボタン.
-            myTwitterButton = UIButton()
-            myTwitterButton.frame = CGRectMake(0,0,100,100)
-            myTwitterButton.backgroundColor = myColor
-            myTwitterButton.layer.masksToBounds = true
-            myTwitterButton.setTitle("Twitter", forState: UIControlState.Normal)
-            myTwitterButton.titleLabel?.font = UIFont.systemFontOfSize(CGFloat(30))
-            myTwitterButton.setTitleColor(UIColor.whiteColor(), forState: UIControlState.Normal)
-            myTwitterButton.layer.cornerRadius = 50.0
-            myTwitterButton.layer.position = CGPoint(x: self.view.frame.width - 55, y:self.view.frame.height - 85)
-            myTwitterButton.tag = 1
-            myTwitterButton.addTarget(self, action: "onPostToTwitter:", forControlEvents: .TouchUpInside)
-            
-            // buttonをviewに追加.
-            self.view.addSubview(myTwitterButton)
         }
       
       
@@ -88,23 +62,6 @@ class GameViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
-    }
-  
-    // ボタンイベント.
-    func onPostToTwitter(sender : AnyObject) {
-      
-      // SLComposeViewControllerのインスタンス化.
-      // ServiceTypeをTwitterに指定.
-      myComposeView = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
-      
-      // 投稿するテキストを指定.
-      myComposeView.setInitialText("Swiftでゲーム動かしたでどやっ！！ #SwiftGirls")
-      
-      // 投稿する画像を指定.
-      myComposeView.addImage(UIImage(named: "SwiftGirls.gif"))
-      
-      // myComposeViewの画面遷移.
-      self.presentViewController(myComposeView, animated: true, completion: nil)
     }
   
 }
